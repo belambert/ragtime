f467e69eaaf31f11669afb274808a9b3bf45552d
Ben Lambert <blambert@gmail.com>
2024-01-27 20:24:12 -0500

    add manual batching; cleanup

--- ragtime/train.py
+++ ragtime/train.py
@@ -1,5 +1,6 @@
+import torch
 import typer
-from datasets import load_dataset
+from datasets import Dataset, load_dataset
 from transformers import RagRetriever, RagTokenForGeneration, RagTokenizer
 from typing_extensions import Annotated
 
@@ -22,43 +23,13 @@ EPOCHS = 1
 def main(debug: Annotated[bool, typer.Option()] = False):
     device = get_device()
     print(device)
-    print("loading tokenizer...")
-    tokenizer = RagTokenizer.from_pretrained("facebook/rag-token-nq")
-    print("loading retriever...")
-    if debug:
-        retriever = RagRetriever.from_pretrained(
-            "facebook/rag-token-nq", index_name="exact", use_dummy_dataset=True
-        )
-    else:
-        retriever = RagRetriever.from_pretrained(
-            "facebook/rag-token-nq",
-            index_name="custom",
-            passages_path="/mnt/disks/data/wiki_dpr",
-            index_path="/mnt/disks/data/wiki_dpr.faiss",
-        )
-    print("loading model...")
-    model = RagTokenForGeneration.from_pretrained(
-        "facebook/rag-token-nq", retriever=retriever
-    )
-    print(model)
-
-    dataset = load_dataset("ms_marco", "v1.1")
-    if debug:
-        dataset["train"] = dataset["train"].select(range(100))
-        dataset["test"] = dataset["test"].select(range(100))
-        dataset["validation"] = dataset["validation"].select(range(100))
-    print(dataset)
-
-    def preprocess(examples):
-        inputs = examples["query"]
-        targets = [ex[0] if len(ex) > 0 else " " for ex in examples["answers"]]
-        model_inputs = tokenizer(
-            inputs, text_target=targets, max_length=MAX_LENGTH, truncation=True
-        )
-        return model_inputs
+    # pylint: disable-next=unused-variable
+    tokenizer, retriever, model = load_model(debug)
+    # tokenizer = RagTokenizer.from_pretrained("facebook/rag-token-nq")
+    dataset = load_ms_marco(debug)
 
     tokenized_dataset = dataset.map(
-        preprocess,
+        get_preproc_function(tokenizer),
         batched=True,
         remove_columns=dataset["train"].column_names,
         num_proc=8,
@@ -73,15 +44,12 @@ def main(debug: Annotated[bool, typer.Option()] = False):
     for i, batch in enumerate(data_iter):
         print(i)
         print(batch)
-
-    # for epoch in range(EPOCHS):
-    #     print(f"epoch: {epoch}")
-    #     # Training
-    #     model.train()
-    #     for i, batch in enumerate(train_dataloader):
-    #         print(f"batch: {i}")
-    #         output = model(**batch)
-    #         print(output.loss)
+        batch["input_ids"] = pad_batch(batch["input_ids"], -100)
+        batch["attention_mask"] = pad_batch(batch["attention_mask"], 0)
+        # we also have `token_type_ids` and `labels`
+        print(batch)
+        output = model(**batch)
+        print(output.loss)
 
     # for epoch in range(EPOCHS):
     #     print(f"epoch: {epoch}")
@@ -101,10 +69,66 @@ def main(debug: Annotated[bool, typer.Option()] = False):
     #         )
     #         result = model(**batch)
     #         print(result.loss)
-
     #         # next thing is to do the weight update...
-
     #         break
+
+
+def load_model(debug: bool = False):
+    print("loading tokenizer...")
+    tokenizer = RagTokenizer.from_pretrained("facebook/rag-token-nq")
+    print("loading retriever...")
+    if debug:
+        retriever = RagRetriever.from_pretrained(
+            "facebook/rag-token-nq", index_name="exact", use_dummy_dataset=True
+        )
+    else:
+        retriever = RagRetriever.from_pretrained(
+            "facebook/rag-token-nq",
+            index_name="custom",
+            passages_path="/mnt/disks/data/wiki_dpr",
+            index_path="/mnt/disks/data/wiki_dpr.faiss",
+        )
+    print("loading model...")
+    model = RagTokenForGeneration.from_pretrained(
+        "facebook/rag-token-nq", retriever=retriever
+    )
+    print(model)
+    return tokenizer, retriever, model
+
+
+def load_ms_marco(debug: bool = False) -> Dataset:
+    dataset = load_dataset("ms_marco", "v1.1")
+    # dataset = load_dataset("ms_marco", "v2.1")
+    if debug:
+        dataset["train"] = dataset["train"].select(range(100))
+        dataset["test"] = dataset["test"].select(range(100))
+        dataset["validation"] = dataset["validation"].select(range(100))
+    print(dataset)
+    return dataset
+
+
+def get_preproc_function(tokenizer):
+    def preprocess(examples):
+        inputs = examples["query"]
+        targets = [ex[0] if len(ex) > 0 else " " for ex in examples["answers"]]
+        model_inputs = tokenizer(
+            inputs, text_target=targets, max_length=MAX_LENGTH, truncation=True
+        )
+        return model_inputs
+
+    return preprocess
+
+
+def pad_batch(vectors: list[torch.Tensor], value) -> torch.Tensor:
+    assert all(map(lambda x: len(x.shape) == 1, vectors))
+    max_len = max(map(lambda x: x.shape[0], vectors))
+    padded = map(lambda x: pad_to_length(x, max_len, value), vectors)
+    return torch.stack(tuple(padded))
+
+
+def pad_to_length(vector: torch.Tensor, length: int, value) -> torch.Tensor:
+    # pylint: disable-next=not-callable
+    return torch.nn.functional.pad(vector, (0, length - len(vector)), value=value)
 
 
 if __name__ == "__main__":
