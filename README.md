70ef12e92eea905f2db3af49423465675f0552aa
Ben Lambert <blambert@gmail.com>
2024-01-28 21:10:10 -0500

    usability improvements; starting a README

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 
+The original RAG implementation is a compelling natural language interface to
+Wikipedia.  The [fine-tuned version available on
+Huggingface](https://huggingface.co/facebook/rag-token-nq) appears to be fine
+tuned on the [Natural Questions dataset](https://huggingface.co/datasets/natural_questions)
+short answers, which means it typically gives very short responses, often
+only 1-3 words long. However, wikipedia contains very rich and detailed information,
+so a RAG model should be able to be fine-tuned to give more detailed answers. In
+this repo, I attempt to fine-tune the original RAG model to produce longer and
+more detailed answers.
+
+Setup
+=====
+If you don't already have python `poetry` installed, run this:
+
+    pip install poetry
+
+Once poetry is installed, you can set up a new virtual environment and install
+wikibot by running:
+
+    poetry install
+
+Using
+=====
+To use, give queries on the command line, like this:
+
+    poetry run python ./ragtime/inference.py "what is a sunflower?"
+
+-----
 
 [Original RAG paper](https://arxiv.org/pdf/2005.11401.pdf)
 
@@ -25,10 +53,9 @@ ms_marco X
 
 Abstractive Question Answering
 
- MSMARCO NLG task v2.1 [43]. The task consists of questions, ten gold passages
+MSMARCO NLG task v2.1 [43]. The task consists of questions, ten gold passages
 retrieved from a search engine for each question, and a full sentence answer annotated from the
 retrieved passages. We do not use the supplied passages, only the questions and answers, to treat
-4
 MSMARCO as an open-domain abstractive QA task. MSMARCO has some questions that cannot be
 answered in a way that matches the reference answer without access to the gold passages, such as
 “What is the weather in Volcano, CA?” so performance will be lower without using gold passages.
--- ragtime/inference.py
+++ ragtime/inference.py
@@ -1,3 +1,6 @@
+from contextlib import redirect_stderr
+from pathlib import Path
+
 import torch
 import typer
 from halo import Halo
@@ -6,6 +9,8 @@ from transformers import RagRetriever, RagTokenForGeneration, RagTokenizer
 from typing_extensions import Annotated
 
 from ragtime.device import get_device
+
+DATA_PREFIX = "/mnt/disks/data/"
 
 
 def main(
@@ -24,46 +29,49 @@ def main(
         "facebook/rag-token-nq",
     )
     print("loading retriever...")
-    retriever = RagRetriever.from_pretrained(
-        "facebook/rag-token-nq",
-        index_name="custom",
-        passages_path="/mnt/disks/data/wiki_dpr",
-        index_path="/mnt/disks/data/wiki_dpr.faiss",
-    )
+    if Path(DATA_PREFIX).exists():
+        with redirect_stderr(None):
+            retriever = RagRetriever.from_pretrained(
+                "facebook/rag-token-nq",
+                index_name="custom",
+                passages_path="/mnt/disks/data/wiki_dpr",
+                index_path="/mnt/disks/data/wiki_dpr.faiss",
+            )
+    else:
+        with redirect_stderr(None):
+            retriever = RagRetriever.from_pretrained(
+                "facebook/rag-token-nq", dataset="wiki_dpr", index_name="compressed"
+            )
     dataset = retriever.index.dataset
-    print(dataset)
     print("loading model...")
-    model = RagTokenForGeneration.from_pretrained(
-        "facebook/rag-token-nq",
-        retriever=retriever,
-    )
-    print("preparing input...")
+    with redirect_stderr(None):
+        model = RagTokenForGeneration.from_pretrained(
+            "facebook/rag-token-nq",
+            retriever=retriever,
+        )
     input_dict = tokenizer.prepare_seq2seq_batch(query, return_tensors="pt")
 
-    # returns RetrievAugLMOutput ?
     with Halo(text="generating...", spinner="dots") as spinner:
-        result = model(**input_dict, output_retrieved=True)
+        result = model(**input_dict, output_retrieved=True)  # RetrievAugLMMarginOutput
+        generated = model.generate(**input_dict)
         spinner.succeed()
-    print(type(result))
-    print(result.logits.shape)
-    token_ids = torch.argmax(result.logits, dim=2)  # -1 dimension?
-    print(token_ids.shape)
-    print(token_ids)
+    print("Answer:", end="")
+    print(tokenizer.batch_decode(generated, skip_special_tokens=True)[0])
     if citations:
         _print_docs(
             result.retrieved_doc_ids, result.doc_scores, dataset, print_passages=sources
         )
-    print(tokenizer.batch_decode(token_ids, skip_special_tokens=True)[0])
 
 
 def _print_docs(doc_ids, doc_scores, dataset, print_passages=False):
     doc_ids = torch.flatten(doc_ids).tolist()
     docs = list(zip(doc_ids, torch.flatten(doc_scores).tolist()))
     docs.sort(key=lambda x: x[1], reverse=True)
+    print(colored("Sources", attrs=["underline"]))
     for id_, score in docs:
         print(f"{dataset[id_]['title']} ({score:.2f})", end="")
         if print_passages:
-            passage = colored(dataset[id_]["text"], "dark_grey")
+            passage = " - " + colored(dataset[id_]["text"], "dark_grey")
             print(passage, end="")
         print()
 
